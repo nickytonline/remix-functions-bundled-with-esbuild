e81e5cd1251ee690e8cb08da8e4dbd532c8b4f02
Nick Taylor <nick@iamdeveloper.com>
2022-06-14 22:13:59 -0400

    chore: created a develop branch to test develop env vars

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,9 @@
 exports.handler = async function (event, context) {
+  const { API_URL = 'default value', DATABASE_URL = 'default value' } =
+    process.env;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    body: JSON.stringify({ API_URL, DATABASE_URL, event, context }),
   };
 };
